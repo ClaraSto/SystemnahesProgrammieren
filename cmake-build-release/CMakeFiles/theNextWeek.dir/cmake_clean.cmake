e4ce23b4ac886bdb4913b1fd52a6fe0b103f26b7
ClaraSto <clara.m.st@outlook.de>
2026-04-20 11:43:11 +0200

    Aufgabe 3c fertig

---
+++ cmake-build-release/CMakeFiles/theNextWeek.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/theNextWeek.dir/link.d"
+  "CMakeFiles/theNextWeek.dir/src/TheNextWeek/main.cc.o"
+  "CMakeFiles/theNextWeek.dir/src/TheNextWeek/main.cc.o.d"
+  "theNextWeek"
+  "theNextWeek.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/theNextWeek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
