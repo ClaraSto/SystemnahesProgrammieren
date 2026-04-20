cdcaaaa11523a7aaaf1fa33336c3bac396c9c6e0
ClaraSto <clara.m.st@outlook.de>
2026-04-20 12:37:28 +0200

    bitte liebes submodul verschwinde

---
+++ Aufgabe1/Aufgabe1/raytracing.github.io/cmake-build-release/CMakeFiles/pi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi.dir/link.d"
+  "CMakeFiles/pi.dir/src/TheRestOfYourLife/pi.cc.o"
+  "CMakeFiles/pi.dir/src/TheRestOfYourLife/pi.cc.o.d"
+  "pi"
+  "pi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
